1ca5c7a2c1e1bca96947058c3aaaeed535d1fc75
Henry Steere <henry.steere@gmail.com>
2021-12-19 12:13:42 +0200

    Day 19 WIP

--- src/day19.hs
+++ src/day19.hs
@@ -1,6 +1,8 @@
 module Day19 where
 
 import Debug.Trace
+import Data.Maybe
+import Data.Foldable
 import Data.Either
 import Text.Parsec.Char
 import Text.Parsec.Combinator
@@ -57,7 +59,7 @@ matchWithDifference is js d =
   let other = S.fromList $ map (\a -> a - d) js 
   in if S.size (S.intersection is other) >= 12 then Right d else Left DontMatch
 
-data CoordType = X | Y | Z deriving Show
+data CoordType = X | Y | Z deriving (Eq, Show)
 
 data Difference = Difference { 
   differenceD :: Int,
@@ -162,16 +164,113 @@ matchesForPair s1 s2 ds = do
   zD <- findZMatches xD yD s1 s2
   return (s1, s2, xD, yD, zD)
 
-data Offset = Offset Int Difference Difference Difference
+data Offset = Offset Int Difference Difference Difference deriving Show
 
-type ScannerOffsets = M.Map Int Offset
+offsetOther :: Offset -> Int
+offsetOther (Offset other _ _ _) = other
+
+type Offsets = M.Map Int [Offset]
 
 allMatches :: [Scanner] -> [(Int, Int, Difference, Difference, Difference)]
 allMatches scns = do
   (s1, s2, xDs) <- findXMatches scns
-  (s1, s2, d1, d2, d3) <- matchesForPair s1 s2 xDs
+  (_, _, d1, d2, d3) <- matchesForPair s1 s2 xDs
   return (scannerId s1, scannerId s2, d1, d2, d3)
+
+flipDifference :: Difference -> Difference
+flipDifference (Difference d f c1 c2) = Difference (-d) f c2 c1
+
+findXDiff :: [Difference] -> Maybe Difference
+findXDiff = L.find ((==X) . differenceCoordOther)
+
+findYDiff :: [Difference] -> Maybe Difference
+findYDiff = L.find ((==Y) . differenceCoordOther)
+
+findZDiff :: [Difference] -> Maybe Difference
+findZDiff = L.find ((==Z) . differenceCoordOther)
+
+flipOffset :: (Int, Int, Difference, Difference, Difference) -> Maybe (Int, Int, Difference, Difference, Difference)
+flipOffset (one, other, d1, d2, d3) = 
+  let ds = [d1, d2, d3]
+  in do
+    xD <- findXDiff ds
+    yD <- findYDiff ds
+    zD <- findZDiff ds
+    return (other, one, flipDifference xD, flipDifference yD, flipDifference zD)
+
+mapOffsets :: [(Int, Int, Difference, Difference, Difference)] -> Offsets
+mapOffsets = foldl' (\mp o@(one, other, xD, yD, zD) -> M.insertWith (++) other [(Offset one xD yD zD)]
+                    $ case flipOffset o of
+                        Nothing -> mp
+                        Just (one2, other2, xD2, yD2, zD2) -> 
+                          M.insertWith (++) other2 [(Offset one2 xD2 yD2 zD2)] mp) M.empty
+
+findOffsetFromZero :: Int -> Offsets -> Maybe [Offset]
+findOffsetFromZero i offsets = findOffsets' S.empty [] i
+  where findOffsets' :: S.Set Int -> [Offset] -> Int -> Maybe [Offset]
+        findOffsets' seen acc curr = 
+          if curr == 0 then Just (reverse acc)
+          else case M.lookup curr offsets of
+            Nothing -> Nothing
+            Just os -> let unseen = filter (\o -> not $ S.member (offsetOther o) seen) os
+                           newSeen = foldl' (\sn o -> S.insert (offsetOther o) sn) seen unseen
+                       in case find isJust $ map (\o -> findOffsets' newSeen (o : acc) (offsetOther o)) unseen of
+                            Nothing -> Nothing
+                            Just path -> path
 
 --part1  = (fmap (map (\(s1, s2, ds) -> (scannerId s1, scannerId s2, ds)) . findXMatches)) <$> input
 
-part1 = (fmap allMatches) <$> input
+findPaths :: [(Int, Int, Difference, Difference, Difference)] -> [(Int, Maybe [Offset])]
+findPaths pairOffsets = 
+  let offsets = mapOffsets pairOffsets
+      coords = S.toList $ foldl' (\st (i, j, _, _, _) -> S.insert i $ S.insert j st) S.empty pairOffsets
+  in map (\i -> (i, findOffsetFromZero i offsets)) coords
+
+combineDifferences :: Difference -> Difference -> Difference
+combineDifferences (Difference d1 f1 cOne1 _) (Difference d2 f2 _ cOther2) =
+  if f1 == -1
+  then Difference (d1 + (f1 * f2 * d2)) f1 cOne1 cOther2
+  else if f2 == 1
+       then Difference (d1 + d2) f1 cOne1 cOther2
+       else Difference (d1 - d2) f1 cOne1 cOther2
+
+
+findXInitDiff :: [Difference] -> Maybe Difference
+findXInitDiff = L.find ((==X) . differenceCoordOne)
+
+findYInitDiff :: [Difference] -> Maybe Difference
+findYInitDiff = L.find ((==Y) . differenceCoordOne)
+
+findZInitDiff :: [Difference] -> Maybe Difference
+findZInitDiff = L.find ((==Z) . differenceCoordOne)
+
+
+combineOffsets :: Offset -> Offset -> Maybe Offset
+combineOffsets (Offset _ dX1 dY1 dZ1) (Offset j dX2 dY2 dZ2) = 
+  let ds = [dX1, dY1, dZ1]
+  in do
+    x <- findXInitDiff ds
+    y <- findYInitDiff ds
+    z <- findZInitDiff ds
+    let c1 = combineDifferences x dX2
+    let c2 = combineDifferences y dY2
+    let c3 = combineDifferences z dZ2
+    let ds2 = [c1, c2, c3]
+    xC <- findXInitDiff ds2
+    yC <- findYInitDiff ds2
+    zC <- findZInitDiff ds2
+    return (Offset j xC yC zC)
+
+combineAllOffsets :: [Offset] -> Maybe Offset
+combineAllOffsets [] = Nothing
+combineAllOffsets (first : rest) =
+  foldl' (\acc o -> case acc of 
+                      Nothing -> Nothing
+                      Just off -> combineOffsets off o) (Just first) rest
+
+offsetsToZero :: [(Int, Int, Difference, Difference, Difference)] -> [(Int, Maybe Offset)]
+offsetsToZero pairOffsets =
+  let paths = findPaths pairOffsets
+  in map (\(i, os) -> (i, os >>= combineAllOffsets)) paths
+
+part1 = (fmap (findPaths . allMatches)) <$> input
