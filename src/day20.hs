2e569f14f2671471d799a47ef13aefa359c3b8cb
Henry Steere <henry.steere@gmail.com>
2021-12-20 21:16:30 +0200

    Day 20 WIP

--- src/day20.hs
+++ src/day20.hs
@@ -19,9 +19,14 @@ positiveInt = read <$> many1 digit
 
 type Algorithm = A.Array Int Char
 
-data Coord = Coord Int Int deriving (Ord, Eq, Show)
+type Pixels = A.Array Int Int
 
-data Image = Image Int Int Int Int (S.Set Coord) deriving Show
+type Indices = A.Array Int Int
+
+data Image = Image Int Int Pixels Indices 
+
+instance Show Image where
+  show i = "\n" ++ imageToString i
 
 data Input = Input Algorithm Image deriving Show
 
@@ -31,20 +36,27 @@ parseAlgorithm = do
   return (A.listArray (0, length alg - 1) alg)
 
 parseRow :: GenParser Char st String
-parseRow = many1 (oneOf ['#', '.'])
+parseRow = (\s -> "...." ++ s ++ "....") <$> many1 (oneOf ['#', '.'])
 
 parseImage :: GenParser Char st Image
 parseImage = do
   first <- (parseRow <* endOfLine)
   rest <- many1 (parseRow <* endOfLine)
   let cs = length first
-  let rs = length rest + 1
-  let allRows = first : rest
-  let withRow = zip [0..rs - 1] allRows
-  let withCol = map (\(i, r) -> (i, zip [0..] r)) withRow
-  let withCoord = map (\(i, r) -> zip (repeat i) r) withCol
-  let justCoords = map (\(r, (c, _)) -> Coord r c) $ filter (\(_, (_, ch)) -> ch == '#') $ concat withCoord
-  return (Image 0 (rs - 1) 0 (cs - 1) (S.fromList justCoords))
+  let emptyRow = take cs $ repeat '.'
+  let rs = length rest + 9
+  let allRows = concat ((emptyRow : emptyRow : emptyRow : emptyRow : first : rest) ++ [emptyRow, emptyRow, emptyRow, emptyRow])
+  let indices = A.listArray (0, rs * cs - 1) $ take (rs * cs) [0..]
+  return $ Image rs cs (A.listArray (0, (rs * cs) - 1) $ map (\c -> if c == '.' then 0 else 1) allRows) indices
+
+toString' :: Int -> String -> String
+toString' _ "" = ""
+toString' cs s = take cs s ++ "\n" ++ toString' cs (drop cs s)    
+
+imageToString :: Image -> String
+imageToString (Image _ cs ps _) = 
+  toString' cs (map (\i -> if i == 1 then '#' else '.') $ A.elems ps)
+
 
 parseInput :: GenParser Char st Input
 parseInput = do
@@ -58,53 +70,36 @@ input = do
   fileContent <- TIO.readFile "/Users/henrysteere/wip/haskell/advent2021/src/day20.input"
   return $ parse parseInput "day20.input" (T.unpack fileContent)
 
-neighbours :: Coord -> [Coord]
-neighbours (Coord r c) = [Coord (r - 1) (c - 1), Coord (r - 1) c, Coord (r - 1) (c + 1), 
-                          Coord r (c - 1), Coord r c, Coord r (c + 1),
-                          Coord (r + 1) (c - 1), Coord (r + 1) c, Coord (r + 1) (c + 1)]
-
 bitsToInt :: [Int] -> Int
 bitsToInt bs = bitsToInt' 0 bs
   where bitsToInt' acc [] = acc
         bitsToInt' acc (b : rest) = bitsToInt' ((acc * 2) + b) rest
 
-findPixelCoordinate :: Image -> Coord -> Int
-findPixelCoordinate (Image _ _ _ _ ps) crd = 
-  let ns = neighbours crd
-  in bitsToInt $ map (\c -> if S.member c ps then 1 else 0) ns
+closeBy :: Image -> Int -> Int -> Bool
+closeBy (Image _ cs _ _) i j = let (r1, c1) = i `divMod` cs
+                                   (r2, c2) = j `divMod` cs
+                               in abs (r1 - r2) <= 1 && abs (c1 - c2) <= 1
+
+neighbours :: Image -> Int -> [Int]
+neighbours im@(Image rs cs _ _) i = filter (\j -> j >= 0 && j < (rs * cs - 1) && closeBy im i j) $
+  [i - cs - 1, i - cs, i - cs + 1, i - 1, i, i + 1, i + cs - 1, i + cs, i + cs + 1]
+
+pixelCoord :: Image -> Int -> Int
+pixelCoord img@(Image _ _ ps _) i = let bits = map (\n -> ps A.! n) $ neighbours img i
+                                    in --trace ("BITS " ++ show bits) $
+                                       bitsToInt bits
 
 nextImage :: Algorithm -> Image -> Image
-nextImage alg im@(Image mnr mxr mnc mxc _) = 
-  let rs = mxr - mnr + 1
-      cs = mxc - mnc + 1
-      (newPs, nMnr, nMnc, nMxr, nMxc) = 
-        foldl' (\acc@(st, newMnr, newMnc, newMxr, newMxc) i -> 
-               let (rInit, cInit) = i `divMod` (cs + 4)
-                   r = rInit + mnr - 2
-                   c = cInit + mnc - 2
-                   nextCrd = (Coord r c)
-                   pixelCoord = findPixelCoordinate im nextCrd
-                   isMember = alg A.! pixelCoord == '#'
-               in if isMember 
-               then-- trace ("NEXT CRD " ++ show nextCrd) $
-                    let nextMnr = newMnr `min` r
-                        nextMnc = newMnc `min` c
-                        nextMxr = newMxr `max` r
-                        nextMxc = newMxc `max` c
-                        nextSt = S.insert nextCrd st
-                    in (nextSt, nextMnr, nextMnc, nextMxr, nextMxc)
-               else acc) (S.empty, mnr, mxr, mnc, mxc) [0..((rs + 4) * (cs + 4) - 1)]
-  in Image (mnr - 2) (mxr + 2) (mnc - 2) (mxc + 2) newPs
+nextImage alg img@(Image rs cs _ is) = 
+  let newPs = fmap (\i -> let pc = pixelCoord img i
+                          in --trace ("I " ++ show i ++ " PC " ++ show pc ++ " ALG PC " ++ show (alg A.! pc)) $
+                             if alg A.! pc == '.' then 0 else 1) is
+  in Image rs cs newPs is
 
-iterImage :: Algorithm -> Image -> Int -> Image
-iterImage _ img 0 = img
-iterImage alg img i = iterImage alg (nextImage alg img) (i - 1)
-
---part1 :: IO (Either ParseError Int)
+--part1 :: IO (Either ParseError Image)
 part1 = do
   parsedInput <- input
   return $ case parsedInput of
     Left err -> Left err
-    Right (Input alg img) -> 
-      let (Image _ _ _ _ ps) = (iterImage alg img 1)
-      in Right ps
+    Right (Input alg img) -> let (Image _ _ final _) = nextImage alg (nextImage alg img)
+                             in Right (foldl' (+) 0 final)
